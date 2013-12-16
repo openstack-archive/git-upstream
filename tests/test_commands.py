25bba73c4361ac9c8a696a65c91055312748e625
Davide Guerri <davide.guerri@hp.com>
2013-12-16 09:46:41 +0000

    Add commands and log unit tests based on python testtools
    JIRA: CICD-1083
    
    Change-Id: I07d2edcd3835a06fc437dab2e02542f7ec013479

---
+++ tests/test_commands.py
@@ -0,0 +1,31 @@
+#
+# Copyright (c) 2012, 2013 Hewlett-Packard Development Company, L.P.
+#
+# Confidential computer software. Valid license from HP required for
+# possession, use or copying. Consistent with FAR 12.211 and 12.212,
+# Commercial Computer Software, Computer Software Documentation, and
+# Technical Data for Commercial Items are licensed to the U.S. Government
+# under vendor's standard commercial license.
+#
+
+"""Tests for the 'commands' module"""
+
+import testtools
+from argparse import ArgumentParser
+
+from ghp import commands as c
+
+class TestGetSubcommands(testtools.TestCase):
+    """Test case for get_subcommands function"""
+
+    _available_subcommands = ('import-upstream',)
+
+    def test_available_subcommands(self):
+        """Test available subcommands"""
+        parser = ArgumentParser()
+        subparsers = parser.add_subparsers()
+        subcommands = c.get_subcommands(subparsers)
+        self.assertEqual(len(TestGetSubcommands._available_subcommands),
+                         len(subcommands.keys()))
+        for command in subcommands.keys():
+            self.assertIn(command, TestGetSubcommands._available_subcommands)
---
+++ tests/test_log.py
@@ -0,0 +1,59 @@
+#
+# Copyright (c) 2012, 2013 Hewlett-Packard Development Company, L.P.
+#
+# Confidential computer software. Valid license from HP required for
+# possession, use or copying. Consistent with FAR 12.211 and 12.212,
+# Commercial Computer Software, Computer Software Documentation, and
+# Technical Data for Commercial Items are licensed to the U.S. Government
+# under vendor's standard commercial license.
+#
+
+"""Tests for then 'log' module"""
+
+import testtools
+from ghp import log as l
+
+
+class TestGetLogger(testtools.TestCase):
+    """Test case for get_logger function"""
+
+    def test_logger_name(self):
+        """Test the default logger name"""
+
+        logger = l.getLogger()
+        self.assertIsNotNone(logger)
+        self.assertEquals('hpgit', logger.name)
+
+    def test_logger_name_param(self):
+        """Test custom logger name"""
+
+        logger = l.getLogger('test')
+        self.assertIsNotNone(logger)
+        self.assertEquals('hpgit.test', logger.name)
+
+
+class TestGetIncrementLevel(testtools.TestCase):
+    """Test case for get_increment_level function"""
+
+    _levels = [
+        ['critical', 'fatal'],
+        ['error'],
+        ['warning', 'warn'],
+        ['notice'],
+        ['info'],
+        ['debug']
+    ]
+
+    def _test_increment_by_x(self, increment=1):
+        """Utility function that tests a given increment"""
+        levels = len(self._levels)
+        for level_no in range(levels-increment):
+            for level in self._levels[level_no]:
+                result = l.getIncrementLevel(1, level)
+                self.assertEquals(self._levels[min(level_no+1, levels-1)][0].upper(),
+                                  result)
+
+    def test_increments(self):
+        """Test all possible increments for all possible default level"""
+        for i in range(len(self._levels)):
+            self._test_increment_by_x(i)
