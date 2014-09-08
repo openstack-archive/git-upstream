d31a0190d668db2002b4f72ba69a1adc000f9e6b
Mike Heald <mike.heald@hp.com>
2014-09-08 11:38:30 +0100

    Make sure dependencies are installed
    Because the console scripts require python-git, the
    requirement is passed to setuptools.setup, and so
    the code is downloaded and the egg in the current
    directory is added to sys.path so the the git-upstream
    script can be installed. But because sys.path can find
    the git module, it doesn't get installed to site-packages.
    So after setup.py runs, we're left with a broken install.
    
    I've added a call to pip to make sure that the requirements
    are installed so that we have a working git-upstream
    script after setup.py finishes.
    
    Change-Id: I122c73223d90c66047bcbbe0e0774094c6693eef

--- setup.py
+++ setup.py
@@ -14,8 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import pip
 import setuptools
 
+pip.main(['install', '-r', 'requirements.txt'])
 
 setuptools.setup(
     setup_requires=['pbr', 'GitPython'],
