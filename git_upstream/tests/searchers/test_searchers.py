11daf57665177cdaae86c326e73b301c267950a3
Darragh Bailey <dbailey@hpe.com>
2015-12-16 17:29:39 +0000

    Begin conversion to testscenarios
    Use testscenarios where possible due to the way parameters within each
    scenario can be accessed directly on the object for providing additional
    feedback in the case of failed tests.
    
    Moves each searcher test into a separate yaml file to be loaded as a
    scenario which helps separate test info and actual code performing the
    test.
    
    Change-Id: Ie86561a8a1b80dc66bfc4bd43bda3ba46b0a5a4a

---
+++ git_upstream/tests/searchers/test_searchers.py
@@ -0,0 +1,55 @@
+# Copyright (c) 2014 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import os
+from pprint import pformat
+import textwrap
+
+from testscenarios import TestWithScenarios
+from testtools.content import text_content
+
+from git_upstream.lib.searchers import UpstreamMergeBaseSearcher
+from git_upstream.tests.base import BaseTestCase
+from git_upstream.tests.base import get_scenarios
+
+
+class TestUpstreamMergeBaseSearcher(TestWithScenarios, BaseTestCase):
+
+    scenarios = get_scenarios(os.path.join(os.path.dirname(__file__),
+                              "scenarios"))
+
+    def setUp(self):
+        super(TestUpstreamMergeBaseSearcher, self).setUp()
+
+        self.addDetail('description', text_content(self.desc))
+        self._build_git_tree(self.tree, self.branches.values())
+
+        # need the tree built at this point
+        self.addDetail('expected-changes',
+                       text_content(pformat(
+                           list((c, self._graph[c].hexsha)
+                                for c in self.expected_changes))))
+
+    def test_search_changes(self):
+        if getattr(self, 'pattern', None):
+            pattern = [self.pattern]
+        else:
+            pattern = None
+
+        searcher = UpstreamMergeBaseSearcher(branch=self.branches['head'][0],
+                                             patterns=pattern, repo=self.repo)
+        self.assertEqual(
+            self._commits_from_nodes(reversed(self.expected_changes)),
+            searcher.list())
