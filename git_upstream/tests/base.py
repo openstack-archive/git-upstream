26c427450b27954526bf2675117695044f7998f1
Darragh Bailey <dbailey@hpe.com>
2016-02-22 23:01:17 +0000

    Consolidate to a single separator character
    Change-Id: I4003b6a71f869b7df30f8bdf0ef219103282665a

--- git_upstream/tests/base.py
+++ git_upstream/tests/base.py
@@ -104,6 +104,9 @@ def get_scenarios(scenarios_path):
                 filename = os.path.join(root, f)
                 with io.open(filename, 'r', encoding='utf-8') as yaml_file:
                     data = yaml.load(yaml_file)[0]
+                # convert any keys with dashes to underscores for easy access
+                data = {key.replace('-', '_'): value
+                        for key, value in data.items()}
                 test_name = os.path.splitext(
                     os.path.relpath(filename, scenarios_path))[0]
                 data['name'] = test_name
@@ -345,7 +348,7 @@ class BaseTestCase(testtools.TestCase):
             self.gittree = BuildTree(
                 self.testrepo, self.tree, self.branches.values())
 
-        if hasattr(self, 'pre-script'):
+        if hasattr(self, 'pre_script'):
             self.run_pre_script()
 
     def run_pre_script(self):
@@ -359,7 +362,7 @@ class BaseTestCase(testtools.TestCase):
         # ensure we execute within context of the git repository
         with DiveDir(self.testrepo.path):
             try:
-                output = subprocess.check_output(getattr(self, 'pre-script'),
+                output = subprocess.check_output(self.pre_script,
                                                  stderr=subprocess.STDOUT,
                                                  shell=True)
             except subprocess.CalledProcessError as e:
