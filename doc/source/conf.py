142eb5ccc67e3cb6340841d1c895d69bcd2fe3be
Darragh Bailey <dbailey@hpe.com>
2016-11-07 18:23:41 +0000

    Use sphinxcontrib-fulltoc
    Change side bar toc to a full toc to should both global and local toc
    simultaneously.
    
    Change-Id: Ied595bab97bccb8cd942c2a0b35a429d7ee88bfb

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -28,7 +28,8 @@ sys.path.insert(0, os.path.abspath('../../git_upstream'))  # for indices
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.todo', 'sphinx.ext.coverage',
+              'sphinxcontrib.fulltoc']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
