fcad8bf8e11d3ef85a0730633105ece739ed2621
Darragh Bailey <dbailey@hpe.com>
2016-11-11 18:14:50 +0000

    Support GitHub cross references using source files
    Allow links to be specified to the source files, which will be
    converted correctly by GitHub, by ensuring the sphinx generation then
    transforms the link from the source file to the correct output file
    depending on the builder used.
    
    Change-Id: I18ac055fc6d44982048365cad819f891786484a8

---
+++ doc/source/transform_github_links.py
@@ -0,0 +1,61 @@
+# based on the idea from
+# https://github.com/andsens/bootstrap-vz/blob/5250f8233215f6f2e3a571be2f5cf3e09accd4b6/docs/transform_github_links.py
+#
+# Copyright 2013-2014 Anders Ingemann <anders@ingemann.de>
+# Copyright 2016 Darragh Bailey <dbailey@hpe.com>
+
+
+from docutils import nodes
+import os.path
+
+
+def transform_github_links(app, doctree, fromdocname):
+    # Convert relative links in repo document source tree directly to
+    # source ReSTructured text documents to links the resulting
+    # document type according to the builder as this allows linking
+    # within the source tree to work correctly for GitHub, while
+    # converting the links correctly for the sphinx generated output.
+
+    target_format = app.builder.link_suffix
+    source_suffix = app.config.source_suffix
+    # app.srcdir is the source directory of the docs, but it's more
+    # important to have the source directory of the current document
+    # the doctree is coming from as links are either relative to
+    # it's path or absolute.
+    doc_path = doctree.attributes['source']
+    doc_dir = os.path.dirname(doc_path)
+
+    for node in doctree.traverse(nodes.reference):
+        if 'refuri' not in node:
+            continue
+        if node['refuri'].startswith('http'):
+            continue
+
+        try:
+            link, anchor = node['refuri'].split('#', 1)
+            anchor = '#' + anchor
+        except ValueError:
+            link = node['refuri']
+            anchor = ''
+
+        if link is None:
+            continue
+
+        # only if ending with source file suffix and referencing a local file
+        # perform the replacement removing the suffix and adding the correct
+        # format ending
+        if link.endswith(source_suffix):
+            # absolute paths are considered relative to repo
+            if link.startswith("/"):
+                basepath = ""
+            # relative paths are against the current doctree source path
+            else:
+                basepath = doc_dir
+            if os.path.exists(os.path.join(basepath, link)):
+                node['refuri'] = (link[:-len(source_suffix)] + target_format +
+                                  anchor)
+
+
+def setup(app):
+    app.connect('doctree-resolved', transform_github_links)
+    return {'version': '0.1'}
